c04c19d4d82fdbb682db9451be64dc9db195ad71
William Schutte <williamdschutte@gmail.com>
2023-09-25 20:17:05 -0400

    problem: Merge Two Sorted Lists

---
+++ pages/problems/mergeTwoSortedLists/index.tsx
@@ -0,0 +1,16 @@
+import { Heading, Text } from "@chakra-ui/react";
+import Layout from "../../layout";
+
+export default function MergeTwoSortedLists() {
+  return (
+    <Layout>
+      <Heading>Merge Two Sorted Lists</Heading>
+      <Text>
+        You are given the heads of two sorted linked lists list1 and list2.
+        Merge the two lists into one sorted list. The list should be made by
+        splicing together the nodes of the first two lists. Return the head of
+        the merged linked list.
+      </Text>
+    </Layout>
+  );
+}
