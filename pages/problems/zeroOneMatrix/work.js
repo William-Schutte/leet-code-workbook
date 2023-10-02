d30508d1be980394d11fdbb662b78f164f9ab25e
William Schutte <williamdschutte@gmail.com>
2023-10-02 18:51:24 -0400

    problem: 01 Matrix

--- pages/problems/zeroOneMatrix/work.js
+++ pages/problems/zeroOneMatrix/work.js
@@ -1,74 +1,56 @@
 /**
- * @param {number[]} nums
+ * @param {number[][]} mat
  * @return {number[][]}
  */
-var threeSum = function (nums) {
-  // Brute force approach. Does not finish in time. :(
-  if (nums.length < 3) return;
-  const answers = {};
-  const testedMap = {};
-
-  for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      if (testedMap[i]?.includes(j)) {
-        continue;
-      }
-      for (let k = j + 1; k < nums.length; k++) {
-        if (testedMap[i]?.includes(j)) {
-          break;
-        }
-        if (nums[i] + nums[j] + nums[k] === 0) {
-          testedMap[i] = testedMap[i] ? [...testedMap[i], j] : [j];
-          const solution = [nums[i], nums[j], nums[k]];
-          const key = solution.sort().toString();
-          answers[key] = solution;
-        }
-      }
-    }
+var updateMatrix = function (mat) {
+  if (!mat || mat.length === 0 || mat[0].length === 0) {
+    return [];
   }
-  arraySolutions = [];
-  for (const value of Object.values(answers)) {
-    arraySolutions.push(value);
-  }
-  return arraySolutions;
-};
 
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum_fast = function (nums) {
-  if (nums.length < 3) return;
-  const results = [];
+  let m = mat.length,
+    n = mat[0].length;
+  let queue = [];
 
-  nums.sort((a, b) => a - b);
-
-  for (let i = 0; i < nums.length - 2; i++) {
-    // Because nums is sorted and we are adding left to right,
-    // nums[i] must be negative or 0 to have a chance of sum == 0
-    if (nums[i] > 0) break;
-    // Once again, because sorted, repeats will be consecutive and can
-    // be skipped
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-    let j = i + 1;
-    let k = nums.length - 1;
-    while (j < k) {
-      let sum = nums[i] + nums[j] + nums[k];
-      if (sum === 0) {
-        results.push([nums[i], nums[j], nums[k]]);
-
-        // Now that we have a solution, we need to skip all the duplicates
-        // and move j and k
-        while (nums[j] === nums[j + 1]) j++;
-        while (nums[k] === nums[k - 1]) k--;
-        j++;
-        k--;
-      } else if (sum < 0) {
-        j++;
+  // The zeros are like mines in minesweeper, so collect them all to iterate over.
+  // Set any non-zero square to infinity.
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (mat[i][j] === 0) {
+        queue.push([i, j]);
       } else {
-        k--;
+        mat[i][j] = Infinity;
       }
     }
   }
-  return results;
+
+  const directions = [
+    [0, 1],
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+  ];
+
+  // For each zero (mine), increase adjacent squares by 1, then save the 1's and repeat
+  // until all squares have been filled in
+  while (queue.length > 0) {
+    const [y, x] = queue.shift();
+    const radiusValue = mat[y][x];
+    for (const [dy, dx] of directions) {
+      // If the adjacent coords are still in the matrix:
+      if (
+        y + dy >= 0 &&
+        y + dy < mat.length &&
+        x + dx >= 0 &&
+        x + dx < mat[0].length
+      ) {
+        // If we haven't set this square yet:
+        if (mat[y + dy][x + dx] === Infinity) {
+          mat[y + dy][x + dx] = radiusValue + 1;
+          queue.push([y + dy, x + dx]);
+        }
+      }
+    }
+  }
+
+  return mat;
 };
